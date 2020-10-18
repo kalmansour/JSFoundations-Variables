a1b84a67d2039f657722533e93625d6e033e67cb
kalmansour <khaledalmansour24@gmail.com>
2020-10-18 20:02:37 +0300

    your message here

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Khaled";
+let age = 24;
+let hobby = "Meditation";
+
+console.log(`Mr.Mrs. ${name} in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
